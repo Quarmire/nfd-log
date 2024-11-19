ed14e8065dc223fad0a536c313df283bbbf174b4
Quarmire <28873711+Quarmire@users.noreply.github.com>
2024-11-19 13:51:35 -0600

    Added download option for constrained platforms

--- install.sh
+++ install.sh
@@ -14,41 +14,84 @@ SERVICE_FILE="/lib/systemd/system/nfd.service"
 CONFIG_FILE="/etc/ndn/nfd.conf"
 RUST_BINARY="nfd_log"
 BUILD_PATH="target/release/$RUST_BINARY"
+NFD_LOG_X86_DOWNLOAD_URL="https://github.com/Quarmire/releases/download/all/nfd_log_x86"
+NFD_LOG_AARCH64_DOWNLOAD_URL="https://github.com/Quarmire/releases/download/all/nfd_log_aarch64"
 INSTALL_PATH="/usr/bin/$RUST_BINARY"
 
-# Build the Rust binary
-echo "Building nfd_log..."
-cargo build --release
+compile_nfd_log() {
+    # Build the Rust binary
+    echo "Building nfd_log..."
+    cargo build --release
+
+    # Install binary to /usr/bin
+    echo "Installing $RUST_BINARY to $INSTALL_PATH..."
+    sudo cp "$BUILD_PATH" "$INSTALL_PATH"
+
+    # Clean up build directory
+    echo "Cleaning up..."
+    rm -r target
+}
+
+# Function to download nfd_log
+download_nfd_log() {
+    ARCH=$(uname -m)
+    if [ "x86_64" = $ARCH ]
+    then
+        echo "Downloading nfd_log binary for $ARCH architecture..."
+        curl -L "$NFD_LOG_X86_DOWNLOAD_URL" -o ./nfd_log || exit 1
+    elif [ "aarch64" = $ARCH ]
+    then
+        echo "Downloading nfd_log binary for $ARCH architecture..."
+        curl -L "$NFD_LOG_AARCH64_DOWNLOAD_URL" -o ./nfd_log || exit 1
+    else
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+    fi
+    sudo chmod +x "$RUST_BINARY"
+    sudo mv ./nfd_log "$INSTALL_PATH"
+    echo "Downloaded and installed nfd_log to $INSTALL_PATH."
+}
 
 # Update the systemd service file
-echo "Modifying $SERVICE_FILE..."
-sudo sed -i.bak \
-    -e 's#^ExecStart=.*#ExecStart=/bin/sh -c "/usr/bin/nfd start --config /etc/ndn/nfd.conf 2>\&1 >/dev/null | /usr/bin/nfd_log"#' \
-    -e 's|^PrivateTmp=.*|PrivateTmp=no|' \
-    "$SERVICE_FILE"
-
-# Reloading system daemon
-echo "Reloading system daemon..."
-sudo systemctl daemon-reload
+update_service_file() {
+    echo "Modifying $SERVICE_FILE..."
+    sudo sed -i.bak \
+        -e 's#^ExecStart=.*#ExecStart=/bin/sh -c "/usr/bin/nfd start --config /etc/ndn/nfd.conf 2>\&1 >/dev/null | /usr/bin/nfd_log"#' \
+        -e 's|^PrivateTmp=.*|PrivateTmp=no|' \
+        "$SERVICE_FILE"
+    
+    # Reloading system daemon
+    echo "Reloading system daemon..."
+    sudo systemctl daemon-reload
+}
 
 # Replace the configuration file
-echo "Replacing $CONFIG_FILE..."
-sudo cp nfd.conf "$CONFIG_FILE"
+replace_nfd_conf() {
+    echo "Replacing $CONFIG_FILE..."
+    sudo cp nfd.conf "$CONFIG_FILE"
+}
 
 # Stop the NFD service
 echo "Stopping NFD service..."
 sudo systemctl stop nfd.service
 
-# Install the binary
-echo "Installing $RUST_BINARY to $INSTALL_PATH..."
-sudo cp "$BUILD_PATH" "$INSTALL_PATH"
+case "$1" in
+    compile)
+        compile_nfd_log
+        ;;
+    download)
+        download_nfd_log
+        ;;
+    *)
+        echo "Usage: $0 {compile|download}"
+        exit 1
+        ;;
+esac
 
+update_service_file
+replace_nfd_conf
 # Restarting the systemd service
 echo "Restarting NFD service..."
 sudo systemctl restart nfd.service
 
-# Clean up build directory
-echo "Cleaning up..."
-rm -r target
-
-echo "Setup complete!"
+echo "Setup completed!"
